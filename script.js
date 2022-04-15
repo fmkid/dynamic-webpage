86dc45a6aad6b4aa0b13344e601912280d2c6bd1
Mao Moreno <86756714+fmkid@users.noreply.github.com>
2022-04-15 12:14:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 function random_value(secret_value) {
-  let rand_number = Math.floor(Math.random() * 62922) + 10;
-  return "Random value is " + (secret_value * rand_number);
+  let rand_number = Math.floor(Math.random() * 50000) + 10;
+  return " " + (secret_value * rand_number);
 }
-
-document.write(random_value(1852));
