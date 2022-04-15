1bed1ac7649a0f1b76c48bf59f659484600bce67
Mao Moreno <86756714+fmkid@users.noreply.github.com>
2022-04-15 00:35:06 -0500

    Upload script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+function random_value(secret_value) {
+  let rand_number = Math.floor(Math.random() * 1000) + 1;
+  return "Random value is " + (10 * secret_value * rand_number);
+}
+
+document.getElementById("result").innerHTML = random_value(1520);
