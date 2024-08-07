21b6d92b9d7811989e9e243ed65ed4e441ebdf14
Mao Moreno <86756714+fmkid@users.noreply.github.com>
2024-08-06 23:10:51 -0500

    Update zoho.js

--- zoho.js
+++ zoho.js
@@ -1,23 +1,19 @@
-var $zoho = $zoho || {}; //NOSONAR
+var $zoho = $zoho || {};
 $zoho.salesiq = $zoho.salesiq || {
-    widgetcode:
-        "siqe94b4a9ea498a6ee876fd2d699d108bc833daf7a018e8a5f32da6a30211afd92",
-    values: {},
-    ready: function () {
-        $zoho.salesiq.chat.department(["Main website"]);
-    },
-};
-$zoho.salesiq.ready = function () {
+  widgetcode:
+    "siqe94b4a9ea498a6ee876fd2d699d108bc833daf7a018e8a5f32da6a30211afd92",
+  values: {},
+  ready: function () {
     $zoho.salesiq.chat.department(["Main website"]);
-    $zoho.salesiq.tracking.on();
-    $zoho.salesiq.chatbutton.visible("show");
-    $zoho.salesiq.chatwindow.visible("show");
+    $zoho.salesiq.chatbutton.visible("hide");
+  },
 };
-var pageDocument = document; //NOSONAR
-secondScript = pageDocument.createElement("script"); //NOSONAR
-secondScript.type = "text/javascript";
-secondScript.id = "zsiqscript";
-secondScript.defer = true;
-secondScript.src = "https://salesiq.zohopublic.com/widget";
-firstScript = pageDocument.getElementsByTagName("script")[0]; //NOSONAR
-firstScript.parentNode.insertBefore(secondScript, firstScript);
+var d = document;
+var s = d.createElement("script");
+s.type = "text/javascript";
+s.id = "zsiqscript";
+s.defer = true;
+s.src = "https://salesiq.zoho.com/widget";
+var t = d.getElementsByTagName("script")[0];
+t.parentNode.insertBefore(s, t);
+d.write("<div id='zsiqwidget'></div>");
